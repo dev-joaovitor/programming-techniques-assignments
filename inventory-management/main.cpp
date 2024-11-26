41a4ddac9b526e6511bf938f39a680f4ddc02714
dev-joaovitor <joaotorvitor1@gmail.com>
2024-11-26 13:38:59 -0300

    `main.cpp` setup products

--- inventory-management/main.cpp
+++ inventory-management/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <cstdint>
+#include <map>
+
+class Product
+{
+private:
+    uint32_t id;
+    std::string name;
+
+public:
+    static uint32_t lastId;
+    uint32_t getId()
+    {
+        return this->id;
+    }
+
+    std::string getName()
+    {
+        return this->name;
+    }
+
+    Product()
+    {
+        this->id = ++this->lastId;
+    }
+
+    ~Product()
+    {
+
+    }
+};
+uint32_t Product::lastId = 0;
 
 int main()
 {
-    std::cout << "Welcome to John's Bakery inventory management software"
-        << '\n';
+    std::map<uint32_t, Product> products;
+    Product *p1 { new Product() };
+    Product *p2 { new Product() };
+
+    std::cout << "p1 id: " << p1->getId() << '\n';
+    std::cout << "p2 id: " << p2->getId() << '\n';
 }
