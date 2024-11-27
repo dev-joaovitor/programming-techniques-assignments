96894b01cadd965d251f8c3c54c3f3c06469c01d
dev-joaovitor <joaotorvitor1@gmail.com>
2024-11-26 22:47:37 -0300

    `main.cpp` products insertion and withdrawal

--- inventory-management/main.cpp
+++ inventory-management/main.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
-#include <cstdint>
-#include <map>
+#include <unordered_map>
 
 class Product
 {
 private:
-    uint32_t id;
+    unsigned int id;
+    unsigned int quantity;
     std::string name;
 
 public:
-    static uint32_t lastId;
-    uint32_t getId()
+    static unsigned int lastId;
+
+    // setters
+    void setName(const std::string& name)
+    {
+        this->name = name;
+    }
+    // getters
+    const unsigned int getId()
     {
         return this->id;
     }
 
-    std::string getName()
+    const std::string& getName()
     {
         return this->name;
     }
 
-    Product()
+    Product(
+        const std::string& name,
+        const unsigned int quantity
+    )
     {
         this->id = ++this->lastId;
+        this->name = name;
+        this->quantity = quantity;
     }
 
     ~Product()
@@ -30,14 +42,82 @@ public:
 
     }
 };
-uint32_t Product::lastId = 0;
+unsigned int Product::lastId = 0;
+
+const void showMenu()
+{
+    std::cout << "\nSystem menu\n";
+    std::cout << "1 - Insert product\n";
+    std::cout << "2 - Update product\n";
+    std::cout << "3 - Withdraw product\n";
+    std::cout << "0 - Exit system\n";
+}
+
+void listProducts(const std::unordered_map<unsigned int, Product*>* products)
+{
+    std::cout << "address of products: " << products << '\n';
+    for (auto& product : *products) {
+        std::cout << product.first
+            << " - " << product.second->getName()
+            << '\n';
+    }
+}
+
+Product* insertProduct()
+{
+    std::string name{};
+    unsigned int quantity{};
+
+    std::cout << "Product name: ";
+    std::cin >> name;
+
+    std::cout << "Product quantity: ";
+    std::cin >> quantity;
+
+    return new Product(name, quantity);
+}
 
 int main()
 {
-    std::map<uint32_t, Product> products;
-    Product *p1 { new Product() };
-    Product *p2 { new Product() };
+    std::unordered_map<unsigned int, Product*> products{};
+    std::cout << "address of products: " << &products << '\n';
 
-    std::cout << "p1 id: " << p1->getId() << '\n';
-    std::cout << "p2 id: " << p2->getId() << '\n';
+    unsigned short choice{0};
+
+    do {
+        showMenu();
+        std::cout << "Select an option: ";
+        std::cin >> choice;
+        std::cout << "\nYour choice: " << choice << '\n';
+
+        switch(choice)
+        {
+            case 1:
+                products[Product::lastId] = insertProduct();
+                break;
+            case 2:
+                listProducts(&products);
+                break;
+            case 3:
+                listProducts(&products);
+                unsigned int productId;
+                std::cout << "Product ID to remove: ";
+                std::cin >> productId;
+                if (products.find(productId) != products.end()) {
+                    products.erase(productId);
+                    std::cout << "\nProduct withdrawn\n";
+                } else std::cout << "\nInvalid product\n";
+                break;
+            case 4:
+                break;
+            case 0:
+                break;
+            default:
+                std::cout << "Invalid option, try again." << '\n';
+        }
+    } while(choice != 0);
+
+    for (auto& product : products) {
+        delete product.second;
+    }
 }
